0b4d1eb42f08cb3c4d446f648231b1e81bc4a5a0
Nick Sarkauskas <nsarka00@gmail.com>
2017-07-14 19:21:45 -0400

    Started Back end

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const express = require('express')
   , http = require('http');
@@ -7,11 +8,45 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+// Serve static files
 app.use('/', express.static('dist'))
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
+app.post('/auth/login', passport.authenticate('local', { successRedirect: '/dash',
+                                                    failureRedirect: '/login' }));
 
+app.post( '/auth/login',
+  passport.authenticate('local'),
 
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 
 server.listen(8080);
